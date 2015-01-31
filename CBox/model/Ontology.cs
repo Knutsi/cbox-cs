ddbe835e41fb1a8db9e4c8fdb35e111ab29eb93c
Knutsi <knutsindre@gmail.com>
2015-01-31 11:38:01 +0100

    Initial commit #2

---
+++ CBox/model/Ontology.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cbox.model
+{
+    public class Ontology
+    {
+        List<Test> Tests;
+        List<Action> Actions;
+
+        public static Ontology LoadFromFile(string path)
+        {
+            throw new Exception("Not implemented");
+        }
+
+        public void SaveToFile(string path)
+        {
+            throw new Exception("Not implemented");
+        }
+    }
+}
---
+++ CBoxTest/model/OntologyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using cbox.model;
+
+namespace CBoxTest
+{
+    [TestClass]
+    public class OntologyTest
+    {
+        [TestMethod]
+        public void LoadFromFile()
+        {
+            
+        }
+
+        [TestMethod]
+        public void SaveToFile()
+        {
+            var ontology = new Ontology();
+            string filepath = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".csv";
+            ontology.SaveToFile(filepath);
+        }
+    }
+}
