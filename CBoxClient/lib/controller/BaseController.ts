8e236728ac2ac56d8b7298934eec0fa945517cbc
Knutsi <knutsindre@gmail.com>
2015-02-11 12:51:51 +0100

    Base controller for sub elements on page

---
+++ CBoxClient/lib/controller/BaseController.ts
@@ -0,0 +1,14 @@
+﻿module cbox.client {
+
+    /* Base class from which the other controllers inherit.  It basically just
+    keeps track of main controller. */
+    export class BaseController {
+
+        page_controller: PlayPageController;
+
+        constructor(page_controller:PlayPageController) {
+            this.page_controller = page_controller;
+        }
+    }
+
+}
