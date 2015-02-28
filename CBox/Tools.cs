2e4316af83fafd0896b7d31e5fba980f52ee5c20
Knutsi <knutsindre@gmail.com>
2015-02-28 17:41:50 +0100

    Powerset support

---
+++ CBox/Tools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cbox
+{
+    public static class Tools
+    {
+        /// <summary>
+        /// Shamelessly ripped from http://rosettacode.org/wiki/Power_set#C.23
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> GetPowerSet<T>(List<T> list)
+        {
+            return from m in Enumerable.Range(0, 1 << list.Count)
+                   select
+                       from i in Enumerable.Range(0, list.Count)
+                       where (m & (1 << i)) != 0
+                       select list[i];
+        }
+    }
+}
