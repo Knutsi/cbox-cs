8840e5a500cc9d3e1b0c93aebf9ca41d48e029aa
Knutsi <knutsindre@gmail.com>
2015-02-28 17:42:05 +0100

    Generic class for XML serializable objects

---
+++ CBox/XMLSerializable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Xml.Serialization;
+
+namespace cbox
+{
+    public class XMLSerializable<T>
+    {
+        public string ToXML()
+        {
+            var writer = new StringWriter();
+            var serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(writer, this);
+
+            return writer.ToString();
+        }
+
+        public static T FromXML(string xml)
+        {
+            if (xml != null && xml != String.Empty)
+            {
+                using (var reader = new StringReader(xml))
+                {
+                    var deser = new XmlSerializer(typeof(T));
+                    var data = (T)deser.Deserialize(reader);
+
+                    return data;
+                }
+            }
+            else
+                return default(T);
+        }
+    }
+}
--- CBox/generation/nodetype/BaseTypeData.cs
+++ CBox/generation/nodetype/BaseTypeData.cs
@@ -8,34 +8,9 @@ using System.Xml.Serialization;
 
 namespace cbox.generation.nodetype
 {
-    public class BaseTypeData
+    public class BaseTypeData : XMLSerializable<BaseTypeData>
     {
         public List<OutputSocket> OutputSockets = new List<OutputSocket>();
 
-        public string ToXML()
-        {
-            var writer = new StringWriter();
-            var serializer = new XmlSerializer(typeof(BaseTypeData));
-            serializer.Serialize(writer, this);
-
-            return writer.ToString();
-        }
-
-        public static BaseTypeData FromXML(string xml)
-        {
-            using (var reader = new StringReader(xml))
-            {
-                var deser = new XmlSerializer(typeof(BaseTypeData));
-                var data = (BaseTypeData)deser.Deserialize(reader);
-
-                return data;
-            }
-        }
-
-        public BaseTypeData()
-        {
-            // default to one output socket:
-            //OutputSockets.Add(new OutputSocket());
-        }
     }
 }
