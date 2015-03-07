2c969c95dcaff206101b056bf94dc8cebcae8669
Knutsi <knutsindre@gmail.com>
2015-03-07 22:14:04 +0100

    TestResult stores all values set

--- CBox/model/TestResult.cs
+++ CBox/model/TestResult.cs
@@ -6,14 +6,28 @@ using System.Threading.Tasks;
 
 namespace cbox.model
 {
+    /// <summary>
+    /// Test result stores the value a key has been set to.  It can have more than 
+    /// one value.  When the value is set multiple times, all values are stored.
+    /// 
+    /// In the event that multiple values are set, but only one should apply, the 
+    /// last value added can be accessed through "Value" (singular).
+    /// </summary>
     public class TestResult
     {
         public string Key { get; set; }
-        public string Value { get; set; }
+        public List<string> Values = new List<string>();
 
-        public TestResult()
+        public string Value
         {
-
+            get
+            {
+                return this.Values.Last();
+            }
+            set
+            {
+                this.Values.Add(value);
+            } 
         }
     }
 }
