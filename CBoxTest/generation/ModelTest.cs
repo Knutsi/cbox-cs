b5ec2d4f2ec4ca3c5fde977280213f589e8035c0
Knutsi <knutsindre@gmail.com>
2015-03-29 16:21:57 +0200

    Test of component add/remove and events

---
+++ CBoxTest/generation/ModelTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using cbox.generation;
+
+namespace CBoxTest
+{
+    [TestClass]
+    public class ModelTest
+    {
+        bool event_compadded_fired = false;
+        bool event_compremoved_fired = false;
+
+        [TestMethod]
+        public void ModelSubmodelsTest()
+        {
+            // creat model and events:
+            var model = new Model(true);
+            model.ComponentAdded += model_ComponentAdded;
+            model.ComponentRemoved += model_ComponentRemoved;
+
+            Assert.AreEqual(0, model.Submodels.Count);
+            Assert.IsTrue(!event_compadded_fired && !event_compremoved_fired);
+
+            // add a component:
+            var A = new NodeCollection();
+            model.AddComponent(A);
+
+            Assert.AreEqual(1, model.Submodels.Count);
+            Assert.IsTrue(event_compadded_fired && !event_compremoved_fired);
+
+            model.RemoveComponent(A);
+            Assert.AreEqual(0, model.Submodels.Count);
+            Assert.IsTrue(event_compadded_fired && event_compremoved_fired);
+        }
+
+        void model_ComponentAdded(NodeCollection collection)
+        {
+            event_compadded_fired = true;
+        }
+
+
+        void model_ComponentRemoved(NodeCollection collection)
+        {
+            event_compremoved_fired = true;
+        }
+
+    }
+}
